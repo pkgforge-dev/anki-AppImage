dabdb494a517cb6c423351bebf7b548dc41a0b53
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-20 02:25:59 -0400

    package is now in the arch repos

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,17 +6,13 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
-
-if [ "$ARCH" = 'x86_64' ]; then
-	pacman -Syu --noconfirm libva-intel-driver
-fi
+pacman -Syu --noconfirm anki
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano intel-media-driver-mini ! llvm-libs
+get-debloated-pkgs --add-common --prefer-nano ! llvm-libs
 
 # Comment this out if you need an AUR package
-make-aur-package anki
+# make-aur-package anki
 
 # If the application needs to be manually built that has to be done down here
